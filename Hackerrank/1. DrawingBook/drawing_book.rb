027c63baa1959be2daee5b8350a4997306cdb4f7
Zeha Irawan <jangkaringbumi@gmail.com>
2019-04-07 16:46:30 +0700

    update indentation

--- Hackerrank/1. DrawingBook/drawing_book.rb
+++ Hackerrank/1. DrawingBook/drawing_book.rb
@@ -1,12 +1,9 @@
 def pageCount(n, p)
+  # front
+  front = (p / 2).floor
 
-    #front
-    front = (p/2).floor
-    
-    #
-    back = (n/2).floor - (p/2).floor
-    
-    result = [front, back].min
-    return result
-    
-    end
+  back = (n / 2).floor - (p / 2).floor
+
+  result = [front, back].min
+  result
+end
